7fc571b52181b6dea6f1e97eed3f28458efbf019
Yashwanth <157216531+Asdfyash1@users.noreply.github.com>
2026-05-19 23:05:02 +0530

    Update echo messages for SSH and ngrok

--- infinite-run.sh
+++ infinite-run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "🚀 Starting Uha Container..."
 /usr/sbin/sshd
-echo "SSH started on port 22"
-echo "Starting ngrok tunnel..."
+echo "✅ SSH Started on port 22"
 ngrok tcp 22 --log=stdout
